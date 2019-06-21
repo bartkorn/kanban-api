e38cc501af41ef12b3c695df9e0f2c62ba1cc47f
bart.korn <bartlomiej.kornowski@gmail.com>
2019-06-21 14:14:42 +0200

    Updated default headers

--- data-api/app.js
+++ data-api/app.js
@@ -11,6 +11,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 //Item
 app.get('/api/item/:id', api.getItem);
